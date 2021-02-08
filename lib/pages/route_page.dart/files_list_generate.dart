7bc4ff326d92c38e57fd579a1e34c4312f4a2acb
yang-lile <liluaya@foxmail.com>
2021-02-08 21:33:07 +0800

    添加返回拦截，抽离文件页

---
+++ lib/pages/route_page.dart/files_list_generate.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:ddm_client/pages/route_page.dart/route_path_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class FilesListGenerate extends StatelessWidget {
+  const FilesListGenerate({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<RoutePathController>(
+      init: RoutePathController(),
+      builder: (routePathController) {
+        var directory = routePathController.directory;
+        var filesList = directory.listSync(followLinks: true);
+        return ListView.separated(
+          itemCount: filesList.length,
+          itemBuilder: (context, index) {
+            var leadingIcon = filesList[index].statSync().type ==
+                    FileSystemEntityType.directory
+                ? Icons.folder_rounded
+                : Icons.insert_drive_file_outlined;
+            return ListTile(
+              leading: Icon(leadingIcon),
+              title: Text(filesList[index].path.split('/').last),
+              onTap: () {
+                routePathController
+                    .inFolder(filesList[index].path.split('/').last);
+              },
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider(
+              height: 1.0,
+            );
+          },
+        );
+      },
+    );
+  }
+}
---
+++ test/test.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+class RoutePathController {
+  Directory directory;
+
+  RoutePathController() {
+    // final box = Hive.box('app_data');
+    this.directory = Directory('/android/app/src');
+  }
+
+  void inFolder(String folderName) {
+    directory = Directory("${directory.path}/$folderName");
+  }
+
+  void outFolder() {
+    directory = directory.parent;
+    // update();
+  }
+}
+
+main(List<String> args) {
+  var filePath = RoutePathController();
+  print(filePath.directory.path);
+  filePath.inFolder('qwe');
+  print(filePath.directory.path);
+  filePath.outFolder();
+  print(filePath.directory.path);
+  filePath.outFolder();
+  print(filePath.directory.path);
+  filePath.outFolder();
+  print(filePath.directory.path);
+}
